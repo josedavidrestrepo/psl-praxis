685982ef1f5d9f252ee5a11f43d498708287110e
josedavidrestrepo <jodrestrepodu@unal.edu.co>
2018-02-28 23:17:58 -0500

    Basic changes

--- praxis/src/components/EnemyOverview/EnemyOverview.js
+++ praxis/src/components/EnemyOverview/EnemyOverview.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './HeroeOverview.css';
+import './EnemyOverview.css';
 
 class HeroeOverview extends Component {
     render() {
-        return (<div className='heroe-overview-container'>
+        return (<div className='enemy-overview-container'>
             <img src={this.props.image} alt={this.props.name} />
             <h1><a href={this.props.link} target="_blank">{this.props.name}</a></h1>
             <p className='enemy-overview-text'>{this.props.overview}</p>
